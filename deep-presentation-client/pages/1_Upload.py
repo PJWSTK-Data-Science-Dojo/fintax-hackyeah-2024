296aa021308672d14465bd60c0b854754ba54e8b
Jakub Melzacki <melzacki.jakub@gmail.com>
2024-09-28 14:55:18 +0200

    Add uploader and env variable

--- deep-presentation-client/pages/1_Upload.py
+++ deep-presentation-client/pages/1_Upload.py
@@ -5,8 +5,8 @@ import streamlit as st
 def upload_viideo():
     st.title("Upload Video")
 
-    uploaded_video = st.file_uploader("Or choose a video...", type=["mp4", "avi", "mov"], key="uploaded_video")
-    
+    uploaded_video = st.file_uploader("Or choose a video...", type=["mp4"], key="uploaded_video")
+
 
 if __name__ == "__main__":
     upload_viideo() 
