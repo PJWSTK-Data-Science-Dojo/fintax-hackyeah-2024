ebe011da61f7b95d547ceb59453e33c3a353c1cc
Franciszek Antoniak <fantoniak@vertabelo.com>
2024-09-29 11:32:09 +0200

    fix

--- deep-presentation-client/pages/2_Analysis_Review.py
+++ deep-presentation-client/pages/2_Analysis_Review.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 import streamlit as st
 from dotenv import load_dotenv
-
+import plotly.express as px
 from utils import api
 from utils.common import initialize
 
